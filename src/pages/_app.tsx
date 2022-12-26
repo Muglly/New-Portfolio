362de9f2d64cd8e7e744fa559286a29d36b217a3
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-25 23:37:05 -0300

    Add Polished and globalstyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 
-import { GlobalStyles } from 'styles/global';
-import { theme } from 'styles/theme';
+import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
