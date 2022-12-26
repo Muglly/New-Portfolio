cc9603de26e63e138a9da509f631575c7ff31bfb
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-25 21:59:57 -0300

    Add Theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,18 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 
 import { GlobalStyles } from 'styles/global';
+import { theme } from 'styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Next.js - Boilerplate</title>
-
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
+        <title>Rafael Souza - Portfolio</title>
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
