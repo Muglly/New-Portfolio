e6ce0209792d4b5e54b65899f3e4444de9903687
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-02 11:11:28 -0300

    Add getStaticPaths in page Project

--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -1,5 +1,51 @@
+import Prismic from '@prismicio/client';
+import { GetStaticPaths, GetStaticProps } from 'next/types';
+import { getPrismicClient } from 'services/prismic';
+import { ProjectsProps } from 'components/SectionProject';
 import { ProjectTemplate } from 'templates/ProjectTemplate';
 
 export default function Project() {
   return <ProjectTemplate />;
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const projects = await prismic.query([
+    Prismic.predicates.at('document.type', 'project'),
+  ]);
+
+  const paths = projects.results.map((project) => ({
+    params: {
+      slug: project.uid,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
+
+  const response = await prismic.getByUID('project', String(slug), {});
+
+  console.log('>>>>>', response);
+
+  // const projects = projectResponse.results.map((project) => ({
+  //   slug: project.uid,
+  //   title: project.data.title,
+  //   type: project.data.type,
+  //   description: project.data.description,
+  //   link: project.data.link.url,
+  //   repositorio: project.data.link.url,
+  //   thumbnail: project.data.thumbnail.url,
+  // }));
+
+  return {
+    props: {},
+    revalidate: 86400,
+  };
+};
